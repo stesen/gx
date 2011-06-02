a36d66550bc021664b77279bd22f842004a15436
stesen <stesenchina@gmail.com>
2011-06-02 11:20:38 +0800

    small cleanning

--- httpd.h
+++ httpd.h
@@ -21,46 +21,45 @@
 #define S1(str) #str
 #define S(str)  S1(str)
 
-#define RFC1123	"%a, %d %b %Y %H:%M:%S GMT"
+#define RFC1123    "%a, %d %b %Y %H:%M:%S GMT"
 
 #define MAXINTERFACES   16
 
 struct DIRCACHE {
-    char             path[1024];
-    char             mtime[40];
-    time_t           add;
-    char             *html;
-    int              length;
-    int              refcount;
-    int              reading;
+    char   path[1024];
+    char   mtime[40];
+    time_t add;
+    char   *html;
+    int    length;
+    int    refcount;
+    int    reading;
 
-    pthread_mutex_t  lock_refcount;
-    pthread_mutex_t  lock_reading;
-    pthread_cond_t   wait_reading;
+    pthread_mutex_t lock_refcount;
+    pthread_mutex_t lock_reading;
+    pthread_cond_t  wait_reading;
 
-    struct DIRCACHE  *next;
+    struct DIRCACHE *next;
 };
 
 struct REQUEST {
-    int	        fd;		     /* socket handle */
-    int	        state;	             /* what to to ??? */
-    time_t      ping;                /* last read/write (for timeouts) */
-    int         keep_alive;
-    int		tcp_cork;
+    int    fd;             /* socket handle */
+    int    state;                 /* what to to ??? */
+    time_t ping;                /* last read/write (for timeouts) */
+    int    keep_alive;
 
     struct sockaddr_storage peer;         /* client (log) */
     char        peerhost[MAX_HOST+1];
     char        peerserv[MAX_MISC+1];
-    
+
     /* request */
-    char	hreq[MAX_HEADER+1];   /* request header */
-    int 	lreq;		      /* request length */
+    char    hreq[MAX_HEADER+1];   /* request header */
+    int     lreq;              /* request length */
     int         hdata;                /* data in hreq */
     char        type[MAX_MISC+1];     /* req type */
     char        hostname[MAX_HOST+1]; /* hostname */
-    char	uri[MAX_PATH+1];      /* req uri */
-    char	path[MAX_PATH+1];     /* file path */
-    char	query[MAX_PATH+1];    /* query string */
+    char    uri[MAX_PATH+1];      /* req uri */
+    char    path[MAX_PATH+1];     /* file path */
+    char    query[MAX_PATH+1];    /* query string */
     int         major,minor;          /* http version */
     char        auth[64];
     struct strlist *header;
@@ -73,14 +72,14 @@ struct REQUEST {
     off_t       *r_end;
     char        *r_head;
     int         *r_hlen;
-    
+
     /* response */
     int         status;              /* status code (log) */
     int         bc;                  /* byte counter (log) */
-    char	hres[MAX_HEADER+1];  /* response header */
-    int	        lres;		     /* header length */
+    char    hres[MAX_HEADER+1];  /* response header */
+    int            lres;             /* header length */
     char        *mime;               /* mime type */
-    char	*body;
+    char    *body;
     off_t       lbody;
     int         bfd;                 /* file descriptor */
     struct stat bst;                 /* file info */
@@ -102,8 +101,7 @@ struct strlist {
 };
 
 /* add element (list head) */
-static void inline
-list_add(struct strlist **list, char *line, int free_the_mallocs)
+static void inline list_add(struct strlist **list, char *line, int free_the_mallocs)
 {
     struct strlist *elem = malloc(sizeof(struct strlist));
     memset(elem,0,sizeof(struct strlist));
@@ -114,16 +112,15 @@ list_add(struct strlist **list, char *line, int free_the_mallocs)
 }
 
 /* free whole list */
-static void inline
-list_free(struct strlist **list)
+static void inline list_free(struct strlist **list)
 {
     struct strlist *elem,*next;
 
     for (elem = *list; NULL != elem; elem = next) {
-	next = elem->next;
-	if (elem->free_the_mallocs)
-	    free(elem->line);
-	free(elem);
+    next = elem->next;
+    if (elem->free_the_mallocs)
+        free(elem->line);
+    free(elem);
     }
     *list = NULL;
 }
@@ -143,7 +140,7 @@ extern time_t now;
 
 static void inline close_on_exec(int fd)
 {
-	fcntl(fd,F_SETFD,FD_CLOEXEC);
+    fcntl(fd,F_SETFD,FD_CLOEXEC);
 }
 
 
@@ -152,13 +149,6 @@ void read_request(struct REQUEST *req, int pipelined);
 void parse_request(struct REQUEST *req);
 
 /* --- response.c ----------------------------------------------- */
-extern char *h200,*h206,*h302,*h304;
-
-extern char *h403,*b403;
-extern char *h404,*b404;
-extern char *h500,*b500;
-extern char *h501,*b501;
-
 void mkerror(struct REQUEST *req, int status, int ka);
 void mkredirect(struct REQUEST *req);
 void mkheader(struct REQUEST *req, int status);
@@ -166,22 +156,22 @@ void write_request(struct REQUEST *req);
 
 /* --- ls.c ----------------------------------------------------- */
 void init_quote(void);
-char*  quote(unsigned char *path, int maxlength);
+char *quote(unsigned char *path, int maxlength);
 struct DIRCACHE *get_dir(struct REQUEST *req, char *filename);
 void free_dir(struct DIRCACHE *dir);
 
 /* --- mime.c --------------------------------------------------- */
 
-char* get_mime(char *file);
-void  init_mime(char *file, char *def);
+char *get_mime(char *file);
+void init_mime(char *file, char *def);
 
 /* -------------------------------------------------------------- */
 
-# define INIT_LOCK(mutex)	pthread_mutex_init(&mutex,NULL)
-# define FREE_LOCK(mutex)	pthread_mutex_destroy(&mutex)
-# define DO_LOCK(mutex)		pthread_mutex_lock(&mutex)
-# define DO_UNLOCK(mutex)	pthread_mutex_unlock(&mutex)
-# define INIT_COND(cond)	pthread_cond_init(&cond,NULL)
-# define FREE_COND(cond)	pthread_cond_destroy(&cond)
-# define BCAST_COND(cond)	pthread_cond_broadcast(&cond);
-# define WAIT_COND(cond,mutex)	pthread_cond_wait(&cond,&mutex);
+# define INIT_LOCK(mutex)      pthread_mutex_init(&mutex,NULL)
+# define FREE_LOCK(mutex)      pthread_mutex_destroy(&mutex)
+# define DO_LOCK(mutex)        pthread_mutex_lock(&mutex)
+# define DO_UNLOCK(mutex)      pthread_mutex_unlock(&mutex)
+# define INIT_COND(cond)       pthread_cond_init(&cond,NULL)
+# define FREE_COND(cond)       pthread_cond_destroy(&cond)
+# define BCAST_COND(cond)      pthread_cond_broadcast(&cond);
+# define WAIT_COND(cond,mutex) pthread_cond_wait(&cond,&mutex);
